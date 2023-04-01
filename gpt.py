54218c87132106d828050e5fbe7b6935229e0b29
Giovanni Vignone <giovanni.vignone@duke.edu>
2023-04-01 16:31:58 -0400

    refining examples

--- gpt.py
+++ gpt.py
@@ -18,7 +18,8 @@ def query_gpt_35_turbo(prompt):
                   {"role": "user", "content": prompt}],
         model="gpt-3.5-turbo"
     )
-    return response.choices[0].message.content
+    text = response.choices[0].message.content
+    return text.lower().replace("\n", "").replace(".", "")
 
 def classify_network_log(log):
     prompt = f"Given the following network log, classify it as normal, suspicious, or malicious. Only return a single word: {log}"
@@ -30,6 +31,8 @@ if __name__ == "__main__":
     network_logs = None
     with open(network_logs_path, 'r') as f:
         network_logs = json.load(f)
-    first_log = network_logs[0]
-    print(classify_network_log(first_log))
+    attack_log = network_logs[0]
+    normal_log = network_logs[1]
+    print(classify_network_log(attack_log))
+    print(classify_network_log(normal_log))
     pass
